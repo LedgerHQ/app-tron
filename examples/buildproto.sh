fdac496d8d804b4b39c2421585ad1cf1b6d3ddf5
Xavier Chapron <xavier.chapron@ledger.fr>
2023-03-10 14:11:16 +0100

    wip

--- examples/buildproto.sh
+++ examples/buildproto.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 mkdir -p protocol/googleapis/google/api
-mkdir proto
+mkdir -p proto
 cp -r ../proto/* proto
 
 git clone https://github.com/tronprotocol/protocol.git protocol/tron
 curl https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/annotations.proto > protocol/googleapis/google/api/annotations.proto
 curl https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/http.proto > protocol/googleapis/google/api/http.proto
 
-python -m grpc_tools.protoc -I. -I./protocol/googleapis --python_out=./proto ./protocol/googleapis/google/api/*.proto ./proto/core/*.proto
+python3 -m grpc_tools.protoc -I. --python_out=. ./proto/core/*.proto
+python3 -m grpc_tools.protoc -I./protocol/googleapis --python_out=./proto ./protocol/googleapis/google/api/*.proto
